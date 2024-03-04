b1780c780354546b9a5341971452e4fc6035bc20
ak800i <belgradbc@gmail.com>
2024-03-04 23:15:17 +0100

    append date to output .tar file

--- build-release.bat
+++ build-release.bat
@@ -1,2 +1,18 @@
-docker build  -t belgradebc/poweroutagenotifier .
-docker save -o poweroutagenotifier.tar belgradebc/poweroutagenotifier
+@echo off
+rem the shenanigans before "docker" commands are here to append the current date to the output filename. 
+setlocal
+
+rem Get current date in YYYY.MM.DD format
+for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set "datetime=%%I"
+set "year=%datetime:~0,4%"
+set "month=%datetime:~4,2%"
+set "day=%datetime:~6,2%"
+
+rem Concatenate the date components to form the filename
+set "filename=poweroutagenotifier-%year%.%month%.%day%.tar"
+
+rem Run Docker commands
+docker build -t belgradebc/poweroutagenotifier .
+docker save -o "%filename%" belgradebc/poweroutagenotifier
+
+endlocal
